afe576e7ad6ede228123682f16e0fc73dd6c7ac0
Jamshidbek <jamshidbekollanazarov@gmail.com>
2023-11-30 18:21:15 +0500

    migration and model created

---
+++ app/Models/Researchers.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Researchers extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'fullname', 
+        'position', 
+        'research_fields', 
+        'photo', 'email', 
+        'facebook_url', 
+        'twitter_url', 
+        'instagram_url',
+    ];
+}
---
+++ database/migrations/2023_11_30_130724_create_researchers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('researchers', function (Blueprint $table) {
+            $table->id();
+            $table->char('fullname');
+            $table->char('position');
+            $table->char('research_fields');
+            $table->char('photo');
+            $table->char('email');
+            $table->char('facebook_url');
+            $table->char('twitter_url');
+            $table->char('instagram_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('researchers');
+    }
+};
