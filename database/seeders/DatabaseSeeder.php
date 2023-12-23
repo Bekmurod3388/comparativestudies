93de1e3ec22c60efdc0c808233512ffa9c63c3eb
Jamshidbek <jamshidbekollanazarov@outlook.com>
2023-12-23 08:48:16 +0500

    contact_user qo'shildi

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             ResearchersTableSeeder::class,
             CarouselsTableSeeder::class,
             FacultyBooksTableSeeder::class,
+            VideosTableSeeder::class,
         ]);
     }
 }
