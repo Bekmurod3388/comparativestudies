25b61c56f179bfaffc0e4c778e6ab5c16053e8ef
Jamshidbek <jamshidbekollanazarov@outlook.com>
2024-02-02 18:58:22 +0500

    researches seeder Rasmlar yangilandi

--- database/seeders/ResearchersTableSeeder.php
+++ database/seeders/ResearchersTableSeeder.php
@@ -19,7 +19,7 @@ class ResearchersTableSeeder extends Seeder
             'research' => "Jahon adabiyoti, Qiyosiy adabiyotshunoslik, G’arb va sharq adabiyotida sayyor syujetlar, Rossiya sharqshunosligi.",
             'scholar' => "https://scholar.google.com/citations?user=XthCZmsAAAAJ&hl=ru&oi=ao",
             'email' => "gulnoz7410@mail.ru",
-            'img' => "researchers/img.png",
+            'img' => "researchers/1.jpg",
             'created_at' => now(),
         ]);
         DB::table('researchers')->insert([
@@ -28,7 +28,7 @@ class ResearchersTableSeeder extends Seeder
             'research' => " romanlar poetikasi, xronotop nazariyasi, arxetip, mega, makro va mikroxronotop, komparativistika.",
             'scholar' => "https://scholar.google.com/citations?hl=ru&user=TuC_b4QAAAAJ&view_op=list_works&gmla=ABEO0YpR7B9HLqguiGm3z0IQ5Sbz0d31IlltnVzRsSgtUBiLssbgZkvpq3RcjjsgQnvjo06zlKkfoLseUykJP_ZPzIM1aiY2ttff4l22HEPSNw",
             'email' => "turayeva.bahor@gmail.com",
-            'img' => "researchers/img_1.png",
+            'img' => "researchers/2.jpg",
             'created_at' => now(),
         ]);
         DB::table('researchers')->insert([
@@ -37,7 +37,7 @@ class ResearchersTableSeeder extends Seeder
             'research' => " O’zbek adabiyoti, zamonaviy jahon va o‘zbek she’riyati, qiyosiy adabiyotshunoslik, adabiyot nazariyasi.",
             'scholar' => "https://scholar.google.com/citations?user=24yQLEcAAAAJ&hl=ru",
             'email' => "zebonur2017@gmail.com",
-            'img' => "researchers/img_2.png",
+            'img' => "researchers/3.jpg",
             'created_at' => now(),
         ]);
         DB::table('researchers')->insert([
@@ -46,21 +46,21 @@ class ResearchersTableSeeder extends Seeder
             'research' => "Qiyosiy adabiyotshunoslik, tilshunoslik, metodika.",
             'scholar' => "https://scholar.google.com/citations?hl=ru&view_op=list_works&gmla=AJ1KiT0Q-vYppEdUJVsWjownHcAY-jB7h2u2Dd6EvYQ5n6h-G-8lxeJCRZtqdZ04iDaHx13rCQScV-g3kldF3054ombdeaLVBSIOXXr9n7IX3xairg80xRfR&user=QkBbEqoAAAAJ",
             'email' => "feruzamannonova@gmail.com",
-            'img' => "researchers/img_3.png",
+            'img' => "researchers/4.jpg",
             'created_at' => now(),
         ]);
         DB::table('researchers')->insert([
             'fullname' => "Yusupova Adolat Anvarovna",
             'position' => "O’qituvchi",
             'research' => "Jahon adabiyoti, rumiyshunoslik, tasavvuf adabiyoti, ingliz va o’zbek didaktik she’riyati.",
-            'img' => "researchers/img_4.png",
+            'img' => "researchers/5.jpg",
             'created_at' => now(),
         ]);
         DB::table('researchers')->insert([
             'fullname' => "Xo’shboqova Gulshoda Kamoliddin qizi",
             'position' => "Talaba",
             'research' => "O’zbek adabiyoti, qiyosiy adabiyotshunoslik, chet tilini o’qitish metodikasi, ingliz romanlari",
-            'img' => "researchers/img_8.png",
+            'img' => "researchers/6.jpg",
             'created_at' => now(),
         ]);
     }
