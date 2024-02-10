646fd6b4b7e54f7f01bc4d626e7bf36379b5c4d2
Jamshidbek <jamshidbekollanazarov@outlook.com>
2024-02-11 02:03:24 +0500

    dashboard sidebar to`g`irlandi

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -27,87 +27,161 @@
             </a>
         </li>
         @if(auth()->user()->hasRole('admin'))
-        <li class="menu-item @if(request()->routeIs('colleagues.index')) active @endif">
-            <a href="{{ route('colleagues.index') }}" class="menu-link">
-                <i class='menu-icon tf-icons bx bx-briefcase-alt'></i>
-                <div data-i18n="Analytics">Hamkasblar</div>
-            </a>
-        </li>
+            <!-- Centers -->
+            <li class="menu-item @if(request()->routeIs('centers.index')) active @endif">
+                <a href="{{ route('centers.index') }}" class="menu-link">
+                    <i class="menu-icon tf-icons bx bxs-bank"></i>
+                    <div data-i18n="Analytics">Markazlar</div>
+                </a>
+            </li>
+            <!-- Locales -->
+            <li class="menu-item @if(request()->routeIs('locales.index')) active @endif">
+                <a href="{{ route('locales.index') }}" class="menu-link">
+                    <i class="menu-icon tf-icons bx bx-world"></i>
+                    <div data-i18n="Analytics">Tillar</div>
+                </a>
+            </li>
+            <!-- About -->
+            <li class="menu-item
+                @if(request()->routeIs('abouts.index')) active @endif
+                @if(request()->routeIs('facultybooks.index')) active @endif
+                @if(request()->routeIs('researcher.index')) active @endif">
+                <a href="javascript:void(0);" class="menu-link menu-toggle">
+                    <i class="menu-icon tf-icons bx bxl-product-hunt"></i>
+                    <div data-i18n="Layouts">Loyiha haqida</div>
+                </a>
 
-        <li class="menu-item @if(request()->routeIs('researcher.index')) active @endif">
-            <a href="{{ route('researcher.index') }}" class="menu-link">
-                <i class='menu-icon tf-icons bx bx-reset'></i>
-                <div data-i18n="Analytics">Tadqiqotchilar</div>
-            </a>
-        </li>
+                <ul class="menu-sub">
+                    <li class="menu-item @if(request()->routeIs('researcher.index')) active @endif">
+                        <a href="{{ route('researcher.index') }}" class="menu-link">
+                            <div data-i18n="Without menu">Tadqiqotchilar</div>
+                        </a>
+                    </li>
+                    <li class="menu-item @if(request()->routeIs('facultybooks.index')) active @endif">
+                        <a href="{{ route('facultybooks.index') }}" class="menu-link">
+                            <div data-i18n="Analytics">Kitoblar</div>
+                        </a>
+                    </li>
+                    <li class="menu-item @if(request()->routeIs('abouts.index')) active @endif">
+                        <a href="{{ route('abouts.index') }}" class="menu-link">
+                            <div data-i18n="Analytics">OAV biz haqimizda</div>
+                        </a>
+                    </li>
+                </ul>
+            </li>
 
-        <li class="menu-item @if(request()->routeIs('dissertations.index')) active @endif">
-            <a href="{{ route('dissertations.index') }}" class="menu-link">
-                <i class='menu-icon tf-icons bx bx-list-check'></i>
-                <div data-i18n="Analytics">Dissertatsiyalar</div>
-            </a>
-        </li>
+            <!-- Scientific -->
+            <li class="menu-item
+                @if(request()->routeIs('dissertations.index')) active @endif
+                @if(request()->routeIs('articles.index')) active @endif">
+                <a href="javascript:void(0);" class="menu-link menu-toggle">
+                    <i class="menu-icon tf-icons bx bx-book-content"></i>
+                    <div data-i18n="Layouts">Ilmiy tadqiqotlar</div>
+                </a>
 
-        <li class="menu-item @if(request()->routeIs('centers.index')) active @endif">
-            <a href="{{ route('centers.index') }}" class="menu-link">
-                <i class="menu-icon tf-icons bx bxs-bank"></i>
-                <div data-i18n="Analytics">Markazlar</div>
-            </a>
-        </li>
+                <ul class="menu-sub">
+                    <li class="menu-item @if(request()->routeIs('dissertations.index')) active @endif">
+                        <a href="{{ route('dissertations.index') }}" class="menu-link">
+                            <div data-i18n="Without menu">Dissertatsiyalar</div>
+                        </a>
+                    </li>
+                    <li class="menu-item @if(request()->routeIs('articles.index')) active @endif">
+                        <a href="{{ route('articles.index') }}" class="menu-link">
+                            <div data-i18n="Analytics">Maqolalar</div>
+                        </a>
+                    </li>
+                </ul>
+            </li>
 
-        <li class="menu-item @if(request()->routeIs('photos.index')) active @endif">
-            <a href="{{ route('photos.index') }}" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-photo-album"></i>
-                <div data-i18n="Analytics">Rasmlar</div>
-            </a>
-        </li>
+            <!-- Magazines -->
+            <li class="menu-item
+                @if(request()->routeIs('journals.index')) active @endif">
+                <a href="javascript:void(0);" class="menu-link menu-toggle">
+                    <i class="menu-icon tf-icons bx bx-bookmarks"></i>
+                    <div data-i18n="Layouts">Ilmiy jurnallar</div>
+                </a>
 
-        <li class="menu-item @if(request()->routeIs('videos.index')) active @endif">
-            <a href="{{ route('videos.index') }}" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-video"></i>
-                <div data-i18n="Analytics">Videolar</div>
-            </a>
-        </li>
+                <ul class="menu-sub">
+                    <li class="menu-item @if(request()->routeIs('journals.index')) active @endif">
+                        <a href="{{ route('journals.index') }}" class="menu-link">
+                            <div data-i18n="Analytics">Jurnallar</div>
+                        </a>
+                    </li>
+                </ul>
+            </li>
 
-        <li class="menu-item @if(request()->routeIs('abouts.index')) active @endif">
-            <a href="{{ route('abouts.index') }}" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-video"></i>
-                <div data-i18n="Analytics">OAV biz haqimizda</div>
-            </a>
-        </li>
+            <!-- Literature -->
+            <li class="menu-item">
+                <a href="javascript:void(0);" class="menu-link menu-toggle">
+                    <i class="menu-icon tf-icons bx bx-book"></i>
+                    <div data-i18n="Layouts">O`quv adabiyotlar</div>
+                </a>
 
-        <li class="menu-item @if(request()->routeIs('carousels.index')) active @endif">
-            <a href="{{ route('carousels.index') }}" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-carousel"></i>
-                <div data-i18n="Analytics">Karusellar</div>
-            </a>
-        </li>
+                <ul class="menu-sub">
+                </ul>
+            </li>
 
-        <li class="menu-item @if(request()->routeIs('facultybooks.index')) active @endif">
-            <a href="{{ route('facultybooks.index') }}" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-book"></i>
-                <div data-i18n="Analytics">Kitoblar</div>
-            </a>
-        </li>
+            <!-- Services -->
+                <li class="menu-item">
+                <a href="javascript:void(0);" class="menu-link menu-toggle">
+                    <i class="menu-icon tf-icons bx bx-server"></i>
+                    <div data-i18n="Layouts">Xizmatlar</div>
+                </a>
 
-        <li class="menu-item @if(request()->routeIs('journals.index')) active @endif">
-            <a href="{{ route('journals.index') }}" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-bookmarks"></i>
-                <div data-i18n="Analytics">Jurnallar</div>
-            </a>
-        </li>
-        <li class="menu-item @if(request()->routeIs('locales.index')) active @endif">
-            <a href="{{ route('locales.index') }}" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-world"></i>
-                <div data-i18n="Analytics">Tillar</div>
-            </a>
-        </li>
-        <li class="menu-item @if(request()->routeIs('articles.index')) active @endif">
-            <a href="{{ route('articles.index') }}" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-world"></i>
-                <div data-i18n="Analytics">Maqolalar</div>
-            </a>
-        </li>
+                <ul class="menu-sub">
+                </ul>
+            </li>
+
+            <!-- Gallery -->
+            <li class="menu-item
+                @if(request()->routeIs('photos.index')) active @endif
+                @if(request()->routeIs('videos.index')) active @endif">
+
+                <a href="javascript:void(0);" class="menu-link menu-toggle">
+                    <i class="menu-icon tf-icons bx bx-photo-album"></i>
+                    <div data-i18n="Layouts">Galereya</div>
+                </a>
+
+                <ul class="menu-sub">
+                    <li class="menu-item @if(request()->routeIs('photos.index')) active @endif">
+                        <a href="{{ route('photos.index') }}" class="menu-link">
+                            <div data-i18n="Analytics">Rasmlar</div>
+                        </a>
+                    </li>
+                    <li class="menu-item @if(request()->routeIs('videos.index')) active @endif">
+                        <a href="{{ route('videos.index') }}" class="menu-link">
+                            <div data-i18n="Analytics">Videolar</div>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+
+            <!-- Contact -->
+            <li class="menu-item">
+                <a href="javascript:void(0);" class="menu-link menu-toggle">
+                    <i class="menu-icon tf-icons bx bxs-contact"></i>
+                    <div data-i18n="Layouts">Aloqa</div>
+                </a>
+
+                <ul class="menu-sub">
+                </ul>
+            </li>
+
+
+{{--        <li class="menu-item @if(request()->routeIs('colleagues.index')) active @endif">--}}
+{{--            <a href="{{ route('colleagues.index') }}" class="menu-link">--}}
+{{--                <i class='menu-icon tf-icons bx bx-briefcase-alt'></i>--}}
+{{--                <div data-i18n="Analytics">Hamkasblar</div>--}}
+{{--            </a>--}}
+{{--        </li>--}}
+{{--        <li class="menu-item @if(request()->routeIs('carousels.index')) active @endif">--}}
+{{--            <a href="{{ route('carousels.index') }}" class="menu-link">--}}
+{{--                <i class="menu-icon tf-icons bx bx-carousel"></i>--}}
+{{--                <div data-i18n="Analytics">Karusellar</div>--}}
+{{--            </a>--}}
+{{--        </li>--}}
+
+
         @endif
         @if(auth()->user()->hasRole('client'))
 
