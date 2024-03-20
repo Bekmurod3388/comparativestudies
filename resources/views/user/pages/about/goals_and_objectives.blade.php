787acd7814f6744d86649aae047657807231ccf0
Jamshidbek <jamshidbekollanazarov@gmail.com>
2024-03-20 13:39:20 +0500

    7 task closed

--- resources/views/user/pages/about/goals_and_objectives.blade.php
+++ resources/views/user/pages/about/goals_and_objectives.blade.php
@@ -21,36 +21,46 @@
                 <div class="blurry-background"></div>
                 <div class="project_start">
                     <div class="project_one">
-                        <h3>Loyihaning maqsadi quyidagilardan iborat:</h3>
-                        <p>Mahalliy va xorijiy mutaxassislarni jalb etgan holda, chet ellarda saqlanayotgan, mamlakatimiz
-                            tarixi va madaniyatiga oid turkiy qoʼlyozmalarni aniqlash, ularning toʼliq maʼlumotlar bazasi —
-                            raqamli qomusini yaratish, uni muntazam yangilab borish, chet el fondlaridagi turkiy
-                            qoʼlyozmalarning faksimile nusxalarini, foto va videotasvirlarini yurtimizga olib kelish,
-                            tegishli ilmiy markazlar, xususan, Аbu Rayhon Beruniy nomidagi Sharqshunoslik instituti bilan
-                            birgalikda ilmiy tadqiqotlar olib borib, erishilgan natijalarni ilmiy muomalaga kiritish
-                            borasidagi ishlarni muvofiqlashtirish; <br> <br> Turkiy qoʼlyozmalarning maʼlumotlar bazasini tiklash jarayonida adabiy manbashunoslik va matnshunoslik faoliyatining boshqa filologik sohalar bilan bogʼliq metodologik asosini aniqlash, adabiy manbalarga editsion tizimda yondashuv qonuniyatlarini oʼrganib, fond bilan ishlashning metod va usullarini taqdim etish, bu sohadagi bilim va tajribalarni takomillashtirishga bogʼliq umumlashmalar turkumini yaratishdan iborat.
+                        <h3>Loyihaning maqsadi va vazifalari quyidagilardan iborat:</h3>
+<p>
+    O‘zbek va jahon adabiyot¬shunosligida  qiyosiy adabiyotshunosik  bo‘yicha amalga oshirilgan tadqiqotlar, fanni o‘zlashtirishga oid barcha materiallarni  o‘zida jamlagan  “Komparativistika” (Comparative)  elektron platformasini yaratish. Ushbu loyiha maqsadii amalga oshirish uchun ilmiy jamoa oldiga qo‘yidagi vazifalar qo‘yilgan:
+    <br>
+    <br>
+    1.  “Komparativistika” (Comparative)  elektron platformasi uchun ilmiy va badiiy manbalarni yig‘ish.  Jumladan, . R. J. Clemens, Pierre Brunel, Claude Pichois, André – Michel Rousseau, N.A.Veselovskiy, N.I.Konrad, V.M.Jirmunskiy, Yu.Lotman,A.Kokorin,  I.G.Neupokoeva, V.Ya.Propp, A.Dima, D.Dyurishin,.M.Baxtin, M.G. Bogatkina. V.R.Amineva, G.  Xallieva, U.Jurakulov, A.Qosimov  kabi  olimlarning rus, ingliz, fransuz, nemis, o‘zbek tillaridagi qiyosiy adabiyotshunoslikka doir ilmiy asarlarini DJVU formatda platformaga yuklash.<br>
+    2.  G.Xallieva, U.Jurakulov birgalikda “Qiyosiy adabiyotshunoslik”  darsligini yaratish va platformaga joylash.<br>
+    3.  Aynan qiyosiy adabiyotshunoslikka doir  O‘zbekistonda va horijda himoya qilingan dissertasiyalarni  jamlash  va DJVU formatda platformaga yuklash.<br>
+    4.  Loyiha doirasida dunyoga mashhur Peter Szondi Institute of Comparative Literature, Freie Universität Berlin, Leibniz-Zentrum für Literatur-und Kulturforschung (ZfL)  kabi kiyosiy adabiyotshunoslik markazlari (Germaniya,Berlin), University of Toronto Centre  for    Comparative Literature(Kanada,Toronto). Yale University  Department of   Comparative Literature(AQSh, Yel' universiteti), Eskişehir Osmangazi Üniversitesi Karşılaştırmalı Edebiyat Bölümü (Turkiya, Osmangozi universiteti)  Harvard University Department of Comparative Literature (AQSh), Indiana University Comparative Literature Department, Columbia University Department of English and Comparative Literature(AQSh), University of Washington Department of Comparative Literature (Vashington)    kabi nufuzli universitetlardagi komparativistika ilmiy markazlari bilan akademik hamkorlik o‘rnatish.<br>
+    5.  Platformani kelajakda tijoratlashtirish, shartnoma asosida  vebinarlar tashkil qilish. <br>
+</p>
+                        {{--                        <p>Mahalliy va xorijiy mutaxassislarni jalb etgan holda, chet ellarda saqlanayotgan, mamlakatimiz--}}
+{{--                            tarixi va madaniyatiga oid turkiy qoʼlyozmalarni aniqlash, ularning toʼliq maʼlumotlar bazasi —--}}
+{{--                            raqamli qomusini yaratish, uni muntazam yangilab borish, chet el fondlaridagi turkiy--}}
+{{--                            qoʼlyozmalarning faksimile nusxalarini, foto va videotasvirlarini yurtimizga olib kelish,--}}
+{{--                            tegishli ilmiy markazlar, xususan, Аbu Rayhon Beruniy nomidagi Sharqshunoslik instituti bilan--}}
+{{--                            birgalikda ilmiy tadqiqotlar olib borib, erishilgan natijalarni ilmiy muomalaga kiritish--}}
+{{--                            borasidagi ishlarni muvofiqlashtirish; <br> <br> Turkiy qoʼlyozmalarning maʼlumotlar bazasini tiklash jarayonida adabiy manbashunoslik va matnshunoslik faoliyatining boshqa filologik sohalar bilan bogʼliq metodologik asosini aniqlash, adabiy manbalarga editsion tizimda yondashuv qonuniyatlarini oʼrganib, fond bilan ishlashning metod va usullarini taqdim etish, bu sohadagi bilim va tajribalarni takomillashtirishga bogʼliq umumlashmalar turkumini yaratishdan iborat.--}}
 
-                            Belgilangan
-                        </p>
+{{--                            Belgilangan--}}
+{{--                        </p>--}}
 
 
                     </div>
 
-                    <div class="project_two">
-                        <h3>Belgilangan maqsadlar quyidagi vazifalarni bajarishni taqozo etadi:</h3>
-                        <p>a) adabiyotshunoslik, tilshunoslik sohalarida manbashunoslik va matnshunoslikning oʼrni va tadqiq obʼektlaridagi tutash nuqtalarni aniqlash; matn tarixi va matn tanqidi tushunchasi; ilmiy-tanqidiy matn tuzishning tasnifiy-qiyosiy mezonlari kabilar haqida nazariy bilim berish;
-                            <br>   <br>
-                            b) mamlakatimiz qoʼlyozma kitoblar fondlaridagi adabiy manbalar toʼla katalogining shartli roʼyxatini shakllantirish;
-                            <br>  <br>
-                            d) shakllantirilgan katalog asosida Yevropa qadimiy kitoblar fondlaridagi adabiy manbalarni qiyosiy oʼrganish;
-                            <br>  <br>
-                            e) Yevropa fondlaridagi adabiy manbalarni tizimli asosda xronologik, tematik tasnif qilish, qoʼlyozma nusxalarining tashqi va ichki xususiyatlaridan kelib chiqib, ularning toʼliq tavsifini tayyorlash;
-                            <br>  <br>
-                            f) loyiha doirasida amalga oshirilgan tadqiqotning tezislar toʼplamini oʼzbek, rus, ingliz tillarida, shuningdek, ularga ilova boʼlgan koʼrgazmali audio va videomateriallarni taqdim etish;
-                            <br>  <br>
-                            g) “Yevroturcologica.uz” elektron platformasining turkiy qoʼlyozma adabiy manbalar toʼla katalogi va qomusiy lugʼatini oʼz ichiga olgan mulьtimedia dasturiy mahsulotlarini yaratish.
-                        </p>
-                    </div>
+{{--                    <div class="project_two">--}}
+{{--                        <h3>Belgilangan maqsadlar quyidagi vazifalarni bajarishni taqozo etadi:</h3>--}}
+{{--                        <p>a) adabiyotshunoslik, tilshunoslik sohalarida manbashunoslik va matnshunoslikning oʼrni va tadqiq obʼektlaridagi tutash nuqtalarni aniqlash; matn tarixi va matn tanqidi tushunchasi; ilmiy-tanqidiy matn tuzishning tasnifiy-qiyosiy mezonlari kabilar haqida nazariy bilim berish;--}}
+{{--                            <br>   <br>--}}
+{{--                            b) mamlakatimiz qoʼlyozma kitoblar fondlaridagi adabiy manbalar toʼla katalogining shartli roʼyxatini shakllantirish;--}}
+{{--                            <br>  <br>--}}
+{{--                            d) shakllantirilgan katalog asosida Yevropa qadimiy kitoblar fondlaridagi adabiy manbalarni qiyosiy oʼrganish;--}}
+{{--                            <br>  <br>--}}
+{{--                            e) Yevropa fondlaridagi adabiy manbalarni tizimli asosda xronologik, tematik tasnif qilish, qoʼlyozma nusxalarining tashqi va ichki xususiyatlaridan kelib chiqib, ularning toʼliq tavsifini tayyorlash;--}}
+{{--                            <br>  <br>--}}
+{{--                            f) loyiha doirasida amalga oshirilgan tadqiqotning tezislar toʼplamini oʼzbek, rus, ingliz tillarida, shuningdek, ularga ilova boʼlgan koʼrgazmali audio va videomateriallarni taqdim etish;--}}
+{{--                            <br>  <br>--}}
+{{--                            g) “Yevroturcologica.uz” elektron platformasining turkiy qoʼlyozma adabiy manbalar toʼla katalogi va qomusiy lugʼatini oʼz ichiga olgan mulьtimedia dasturiy mahsulotlarini yaratish.--}}
+{{--                        </p>--}}
+{{--                    </div>--}}
                 </div>
             </div>
         </div>
