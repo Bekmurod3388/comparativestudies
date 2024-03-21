fa372d6b3deb62f36edd4741785a5a29cb3718c2
XayrulloWeb <xayrulloweb@gmail.com>
2024-03-21 12:21:47 +0300

    add loader

---
+++ public/assets_front/js/loader.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+    // Скрыть анимацию загрузки после загрузки страницы
+    const loaderWrapper = document.querySelector('.loader-wrapper');
+    loaderWrapper.style.display = 'none';
+});
