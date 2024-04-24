164d1f8bd24d52ef98c5d5bf21892addd931f813
XayrulloWeb <xayrulloweb@gmail.com>
2024-04-24 17:29:17 +0500

    add abouPlatform

--- resources/views/user/pages/about/aboutThePlatform.blade.php
+++ resources/views/user/pages/about/aboutThePlatform.blade.php
@@ -1,4 +1,61 @@
 @extends('user.layouts.master')
 @section('content')
-    <h1>dom</h1>
+<div class="goals">
+        <div class="goals_start">
+            <div class="container text-center animated bounceInDown">
+                <h1 class="display-1 mb-4">KOMPARATIVISTIKA” ELEKTRON PLATFORMASI HAQIDA</h1>
+                <ol class="breadcrumb justify-content-center mb-0 animated bounceInDown">
+                    <li class="breadcrumb-item"><a href="#">Asosiy sahifa </a></li>
+                    <li class="breadcrumb-item"><a href="#">Loyiha haqida</a></li>
+                    <li class="breadcrumb-item text-dark" aria-current="page">Platforma haqida</li>
+                </ol>
+            </div>
+        </div>
+        <!-- Hero End -->
+
+
+        <!-- Testimonial Start -->
+        <!-- LOYIHA START-->
+        <div class="project">
+            <div class="container">
+                <div class="blurry-background"></div>
+                <div class="project_start">
+                    <div class="project_one">
+                        <div class="platforma_text">
+                            <p>
+                                <span>Xallieva Gulnoz Iskandarovna </span>
+, filologiya fanlari doktori, professor, “Jahon adabiyoti” kafedrasi mudiri, loyiha rahbari.
+
+“Komparativistika” elektron platformasi dasturchilari
+
+
+
+Ma’lumki, bugungi kunda startap va dolzarb mavzudagi ilmiy loyihalar, davlatimiz tomonidan qoʼllab quvvatlanmoqda. 2023 yilda jahon adabiyoti kafedramiz ilmiy jamoasi bilan “Qiyosiy adabiyotshunoslik” fanining “Komparativistika” elektron platformasini yaratish” mavzusidagi innovatsion loyiha boʼyicha grant sohibi boʼldik. Mazkur loyihaning obʼekti – qiyosiy adabiyotshunoslik, maqsadi oʼzbek va jahon adabiyotshunosligida qiyosiy adabiyotshunosik boʼyicha amalga oshirilgan tadqiqotlar, fanni oʼzlashtirishga oid barcha materiallarni oʼzida jamlagan “Komparativistika” elektron platformasini yaratishdan iborat. 
+Qiyosiy adabiyotshunoslik yoki adabiy komparativistika ikki va undan ortiq adabiy hodisaning qiyos lanishiga asoslangan fan yoʼnalishidir. Xalqaro ijtimoiy, madaniy va adabiy aloqalar kundan kunga ri¬vojlanayotgan zamonamizda qiyosiy adabiyotshunoslik kelajagi yorugʼ fan yoʼnalishlaridan biridir. Har qanday qiyosiy tadqiqotda adabiy hodisalarning mushtarak va oʼziga xos jihatlari aniqlanadi. Bu esa adabiyotlararo umumiy nazariy qonuniyatlarning yuzaga kelishida asos boʼlib xizmat qiladi. Badiiy asarlarni qiyosan oʼrganish metodologiyasi va tamoyillari boʼyicha Аmerika, Yevropa va Rossiyada juda koʼplab darslik, oʼquv qoʼllanma, veb sahifalar yaratilgan. Аmmo Oʼzbekistonda hali hanuz bu borada maxsus elektron platforma yaratilmagan. “Qiyosiy adabiyotshunoslik” fanining “Komparativistika” elektron platformasidan barcha jahon adabiyoti va qiyosiy adabiyotshunoslik masalalari bilan qiziquvchi keng kitobxonlar ommasi unumli foydalanishi mumkin. Umuman, ushbu loyiha natijalari nafaqat ilm fan taraqqiyotiga, balki xalkimiz orasida maʼnaviyatning yuksalishi, kitobxonlik madaniyatini rivojlantirishga, bu bilan bogʼliq hukumatimiz qarorlarining ijrosini taʼminlanishiga, jahon xalqlari tarixi, madaniyati, adabiyoti bilan yaqindan tanishtirishga xizmat qiladi.   
+“Loyihada qoʼyilgan ilmiy muammo OʼzR Vazirlar Mahkamasining «Jahon adabiyotining eng sara namunalarini oʼzbek tiliga hamda oʼzbek adabiyoti durdonalarini chet tillariga tarjima qilish va nashr etish tizimini takomillashtirish chora-tadbirlari toʼgʼrisida»gi 2018 yil 18 maydagi 376-sonli, “Internet jahon axborot tarmogʼida milliy kontentni yanada rivojlantirish chora-tadbirlari toʼgʼrisida” 2017 yil 14 avgustdagi VMQ-625-sonli qarorlarida hamda kitobxonlik madaniyatini oshirishga qaratilgan barcha meʼyoriy-huquqiy hujjatlarda belgilangan vazifalarni amalga oshirishga xizmat qiladi. Respublikamizning koʼpgina universitetlarida - «Qiyosiy adabiyotshunoslik, chogʼishtirma tilshunoslik va tarjimashunoslik» (10.00.06) ixtisosligi boʼyicha Ilmiy kengashlar mavjudligi, har yili oʼnlab ilmiy tadqiqotlar qilinayotganiga qaramay, oʼquv-metodik manba yara¬tish masalasi dolzarb boʼlib qolmoqda. Natijada qiyosiy adabiyotshunoslik metodologiyasini yaxshi oʼzlashtirmasdan turib, ilmiy ishlar yuzaga kelmoqda. Bu esa komparativistika yoʼnalishidagi tadqiqotlarning umumiy saviyasiga taʼsir qilmoqda. Shu jihatdan, loyiha davomida qoʼlga kiritiladigan ilmiy yutuqlar va natijalar kelgusida ushbu yoʼnalishda amalga oshiriladigan ilmiy tadqiqot ishlari uchun asos boʼlib xizmat qiladi. 
+
+
+ “Komparativistika” elektron platformasi quyidagi kontentlardan iborat boʼlishi rejalashtirilgan. Platformani bosh sahifasida , globus atrofida dunyoning eng mashhur, Gʼarb va Sharq komparativistika markazlari haqida giperhavola berilgan. Unda dunyoga mashhur Peter Szondi Institute of Comparative Literature, Freie Universität Berlin, Leibniz-Zentrum für Literatur-und Kulturforschung (ZfL) kabi kiyosiy adabiyotshunoslik markazlari (Germaniya,Berlin), University of Toronto Centre for Comparative Literature(Kanada,Toronto). Yale University Department of Comparative Literature(АQSh, Yelь universiteti), Eskişehir Osmangazi Üniversitesi Karşılaştırmalı Edebiyat Bölümü (Turkiya, Osmangozi universiteti) Harvard University Department of Comparative Literature (АQSh), Indiana University Comparative Literature Department, Columbia University Department of English and Comparative Literature(АQSh), University of Washington Department of Comparative Literature (Vashington) kabi nufuzli universitetlardagi komparativistika ilmiy markazlari, kafedralari haqida bilib olish mumkin. Bu sohaning jahon miqyosida qanchalik rivojlanganligi haqida tassurot va boy maьlumot beradi.</p>
+ <img src="{{asset('/assets_front/img/platforma1.png')}}" alt="">
+ <p>Loyiha haqida qismida aynan loyihaning oʼzigagina tegishli materiallar joy olgan, yaьni, loyihaning maqsad va vazifalar, ishtirokchilari, ular tomonidan yaratilgan maqola va kitoblar, OАV larida loyiha haqidagi , intervьyu, koʼrsatuvlar, videolavhalar mavjud. Bundan tashqari, bu boʼlimda loyiha rejasi asosida yangi tashkil qilingan “Komparativistika” jurnali, talablari va maqolani onlayn yuborish tartibi haqida axborot berilgan. Ushbu jurnal yilda 4 marta onlayn nashr qilinadi, «Qiyosiy adabiyotshunoslik, chogʼishtirma tilshunoslik va tarjimashunoslik» (10.00.06) ixtisosligi boʼyicha barcha maqolalarni qabul qiladi.</p>
+ <img src="{{asset('/assets_front/img/platforma2.png')}}" alt="">
+ <p> <span>Platformaning ikkinchi kontenti “Ilmiy tadqiqotlar” deb nomlanadi.</span>  Unda oʼzbek rus, ingliz va boshqa tillardagi izlab topilgan, qiyosiy adabiyotshunoslikka oid dissertatsiyalar, avtoreferatlar, monografiyalar va eng dolzarb maqolalar joylashtiriladi. Аynan shu kontent tadqiqotchilar uchun juda foydali boʼlib, bu orqali izlanuvchilar oʼz sohasi boʼyicha turdosh tadqiqotlarni oʼrganadi, dissertatsiyasida takrorlar boʼlishini oldini oladi va oʼz sohasining yetuk bilimdoni boʼlish uchun bilim va malakasini oshiradi. 
+ <br> <span>Uchinchi “Ilmiy jurnallar” deb nomlangan boʼlimda</span>  qiyosiy adabiyotshunoslik boʼyicha Аmerika, Rossiya, Turkiya, Yevropa va boshqa davlarlardagi dunyo jurnallari jamlangan. Bu mediamaydon tadqiqotchilar uchun ulkan imkoniyat manbai boʼlib, ular jahon standartlari asosida maqola tayyorlash va nashr qilish, dunyoning nufuzli universitetlari olimlari, kafedralari, markazlari bilan akademik hamkorlik oʼrnatish imkoniyatini yaratadi
+</p>
+<img src="{{asset('/assets_front/img/platforma3.png')}}" alt="">
+<p>       <span> Toʼrtinchi kontentda komparativistika</span>    boʼyicha oʼquv adabiyotlari jamlangan. Qiyosiy adabiyotshunoslik boʼyicha respublikada atigi 3 ta oʼquv qoʼllanma (G. Xallieva, U.Jurakulov, А.Qosimov) yaratilganini xisobga olsak, aynan ushbu kontentda ilmiy-oʼquv adabiyotlar tanqisligiga bilan bogʼliq muammolar hal qilinadi. Unda nafaqat MDH davlatlari balki, dunyo miqyosida yaratilgan rus, ingliz, frantsuz, nemis tillaridagi darslik, oʼquv qoʼllanma, metodik qoʼllanmalarni oʼqib, oʼrganish imkoniyati yaratilgan. Platformada shuningdek, anʼanaviy barcha elektron platformalarda boʼlgani kabi foto va video galereya , qayta aloqa qismlari ham mavjud.
+ 
+ Mazkur platformada vebinarlar tashkil etish imkoni keyinchalik ushbu platformani tijoratlashtirish imkonini yaratadi. Shuningdek, loyiha tugagandan keyin ham davomli ravishda loyiha doirasida yaratilgan oʼquv adabiyotlarini moliyaviy shartnomalar asosida realizatsiya qilishga keng yoʼl ochadi. Shuning uchun ham platformada xizmatlar deb atalgan kontent ham joriy qilingan. Bu media orqali (10.00.06) ixtisosligi boʼyicha kirish va malakaviy imtixonlarga tayyorlash, ochiq maьruzalar tashkil qilish, loyihani tijoratlashtirish masalasi ham rejalashtirilgan.
+Loyiha Oʼzbekiston Respublikasining iqtisodiy oʼsishi va ijtimoiy rivojlanishini taʼminlaydigan ilmiy va texnologik zaxiralarni shakllantirish bilan bogʼliq boʼlib, kelajakda ulkan samara beradi Chunki loyiha natijasida yangi kadrlar va yangi dasturiy mahsulotlar shakllanadi, jamiyat taraqqiyotiga xizmat qiladigan intelektual mulk yaratiladi.
+
+ “Qiyosiy adabiyotshunoslik” fanining “Komparativistika” elektron platformasini yaratish innovatsion loyihasida ijrochi tashkilot - Oʼzbekiston davlat jahon tillari universitetiti tarkibida “Qiyosiy adabiyotshunoslik, chogʼishtirma tilshunoslik va tarjimashunoslik» (10.00.06) ixtisosligi boʼyicha Ilmiy kengashning mavjudligi, turli tillarda dars bera oladigan mutaxassislarning borligi, kelajakda loyihaning bardavom boʼlishini va turli davlatlarning nufuzli markazlari bilan akademik aloqa oʼrnatishni taʼminlaydi.
+</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
 @endsection
