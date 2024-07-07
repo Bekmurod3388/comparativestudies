1fc58aa2dca26f82f45a6372a3b5eb237ec39e17
Jamshidbek <jamshidbekollanazarov@gmail.com>
2024-07-08 01:00:14 +0500

    jurnallarning yangi seederlari qo'shildi

--- database/seeders/ResearchersArticlesTableSeeder.php
+++ database/seeders/ResearchersArticlesTableSeeder.php
@@ -14,16 +14,6 @@ class ResearchersArticlesTableSeeder extends Seeder
     public function run(): void
     {
         $abstracts = [
-//            [
-//                'article_topic' => 'XORAZM MA`MUN AKADEMIYASI AXBOROTNOMASI',
-//                'authors' => 'Abdullayev Ikram Iskandarovich',
-//                'article_type' => "Oak",
-//                'protection_year' => '2022-10/4',
-//                'file_url' => 'researcher_articles/files/Mamun vak maqola.pdf',
-//                'photo_url' => "researcher_articles/photos/img.png",
-//                'created_at' => now(),
-//                'updated_at' => now(),
-//            ],
             [
                 'article_topic' => 'About The Manuscript Of "Ma\'arifun Nasab" By Babajon Sanoy, The Single Copy Of Which Is Kept In The State Library Of Berlin',
                 'authors' => 'Xalliyeva Gulnoz Iskandarovna, Zohidov Rashid Fozilovich, Adambaeva Nargiza Kadambaevna',
